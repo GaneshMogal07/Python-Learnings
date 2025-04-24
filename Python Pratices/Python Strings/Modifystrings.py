4d37c06cc1bb77754723a77db5a47b01fd7db9df
GaneshMogal07 <ganeshmogal35@gmail.com>
2025-04-24 19:11:07 +0530

    latest

---
+++ Python Pratices/Python Strings/Modifystrings.py
@@ -0,0 +1,19 @@
+a = "Hello, World!"
+print(a.upper())
+print(a.lower())
+
+""" Output-
+C:\Users\GaneshMogal\Python-Learnings\Python Pratices\Python Strings>python Modifystrings.py
+HELLO, WORLD!
+hello, world!
+
+"""
+
+
+
+# a = "Hello, World!"
+# print(a.replace("H", "J"))
+
+
+# -output:-
+# Jello, World!

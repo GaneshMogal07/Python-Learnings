e64eed1ac23a50696f5c6ef9f267c256e395ceb6
GaneshMogal07 <ganeshmogal35@gmail.com>
2025-04-24 18:50:07 +0530

    latest

---
+++ Python Pratices/Python Strings/Slicing.py
@@ -0,0 +1,18 @@
+# b = "Hello, World!"
+# print(b[:2])
+
+# #this is slicing used for left saides .
+
+# """ output-
+#  C:\Users\GaneshMogal\Python-Learnings\Python Pratices\Python Strings>python Slicing.py 
+#   He          """
+
+
+b = "Hello, World!"
+print(b[2:])
+
+"""
+output-
+C:\Users\GaneshMogal\Python-Learnings\Python Pratices\Python Strings>python Slicing.py 
+llo, World!
+"""
---
+++ Python Pratices/Python Strings/multilinecomm.py
@@ -0,0 +1,17 @@
+a = """Lorem ipsum dolor sit amet,
+consectetur adipiscing elit,
+sed do eiusmod tempor incididunt
+ut labore et dolore magna aliqua."""
+print(a)
+
+"""output-
+C:\Users\GaneshMogal\Python-Learnings\Python Pratices\Python Strings>python multilinecomm.py
+Lorem ipsum dolor sit amet,
+consectetur adipiscing elit,
+sed do eiusmod tempor incididunt
+ut labore et dolore magna aliqua.
+
+"""
+
+a = "Hello, World!"
+print(a[0])

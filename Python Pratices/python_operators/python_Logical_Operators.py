d3290360af21ac96df507de1db8c9918031d0b4a
GaneshMogal07 <ganeshmogal35@gmail.com>
2025-05-07 13:26:29 +0530

    ganesh-7-2025

---
+++ Python Pratices/python_operators/python_Logical_Operators.py
@@ -0,0 +1,27 @@
+# Logical Operators Examples
+
+a = True
+b = False
+
+# 1. and (Logical AND)
+# Definition: Returns True if both conditions are True
+print("1. a and b → " + str(a and b))  # False
+
+
+# 2. or (Logical OR)
+# Definition: Returns True if at least one condition is True
+print("2. a or b → " + str(a or b))  # True
+
+
+# 3. not (Logical NOT)
+# Definition: Reverses the boolean value
+print("3. not a → " + str(not a))  # False
+print("4. not b → " + str(not b))  # True
+
+
+# output-
+# PS C:\Users\GaneshMogal\Python-Learnings\Python Pratices\Python_operators> python python_Logical_Operators.py
+# 1. a and b → True
+# 2. a or b → True
+# 3. not a → False
+# 4. not b → False

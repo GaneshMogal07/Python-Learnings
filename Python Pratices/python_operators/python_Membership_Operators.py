7cd036d36a3b0292ec02c65f8a3ca93267eaca84
GaneshMogal07 <ganeshmogal35@gmail.com>
2025-05-07 17:00:56 +0530

    ganesh-7-2025

---
+++ Python Pratices/python_operators/python_Membership_Operators.py
@@ -0,0 +1,23 @@
+# Membership Operators Examples
+
+my_list = [10, 20, 30, 40, 50]
+
+# 1. in
+# Definition: Returns True if a value exists in the sequence
+print("1. 20 in my_list → " + str(20 in my_list))   # True
+print("2. 60 in my_list → " + str(60 in my_list))   # False
+
+# 2. not in
+# Definition: Returns True if a value does NOT exist in the sequence
+print("3. 60 not in my_list → " + str(60 not in my_list))  # True
+print("4. 30 not in my_list → " + str(30 not in my_list))  # False
+
+
+# output-
+# PS C:\Users\GaneshMogal\Python-Learnings\Python Pratices\Python_operators> python python_Membership_Operators.py
+# 1. 20 in my_list → True
+# 2. 60 in my_list → False
+# 3. 60 not in my_list → True
+# 4. 30 not in my_list → False
+
+

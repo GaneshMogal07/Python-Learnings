494b83e120ae48f830d9db9e6a44abf0ec7f8dc0
GaneshMogal07 <ganeshmogal35@gmail.com>
2025-05-07 09:23:38 +0530

    ganesh-7-2025

---
+++ Python Pratices/python_operators/Python_Arithmetic_Operators.py
@@ -0,0 +1,106 @@
+# """ 1. Addition (+)
+#  Definition: It is used to add two or more numbers.
+#  Example:
+
+a = 10
+b = 5
+result = a + b
+print("Addition:", result) 
+
+# PS C:\Users\GaneshMogal\Python-Learnings\Python Pratices\python_operators> python Python_Arithmetic_Operators.py
+# Addition: 15"""
+
+
+
+
+# 2. Subtraction (-)
+# Definition: It is used to subtract the second number from the first.
+# Example:
+    
+a = 10
+b = 5
+result = a - b
+print("Subtraction:", result)  
+
+# PS C:\Users\GaneshMogal\Python-Learnings\Python Pratices\python_operators> python Python_Arithmetic_Operators.py
+# Subtraction: 5
+
+
+
+
+
+# 3. Multiplication (*)
+# Definition: It is used to multiply two numbers.
+# Example:
+
+
+a= 10
+b= 4
+result = a * b
+print("multiplication:",result)
+
+# PS C:\Users\GaneshMogal\Python-Learnings\Python Pratices\python_operators> python Python_Arithmetic_Operators.py
+# multiplication: 401     
+
+
+
+
+
+# 4. Division (/)
+# Definition: It divides the first number by the second and returns a float value.
+# Example:
+
+a= 10
+b= 5
+result = a / b
+print("Division is:",result)
+
+# PS C:\Users\GaneshMogal\Python-Learnings\Python Pratices\python_operators> python Python_Arithmetic_Operators.py
+# Division is: 2.0
+
+
+
+
+# 5. Floor Division (//)
+# Definition: It divides the number and returns the integer (whole number) part.
+# Example:
+a = 10
+b = 5
+result = a // b
+print("Floor Division:", result)  # Output: 3
+
+# PS C:\Users\GaneshMogal\Python-Learnings\Python Pratices\Python_operators> python Python_Arithmetic_Operators.py
+# Floor Division: 2
+
+
+
+# 6. Modulus (%)
+# Definition: It returns the remainder after division.
+# Example:
+
+a = 10
+b = 4
+result = a % b
+print("Modulus:", result)  # Output: 1
+#expanation is when you used modules opratores calcultion is 4*4=8 and 10-8=2 this the final output.
+
+# PS C:\Users\GaneshMogal\Python-Learnings\Python Pratices\Python_operators> python Python_Arithmetic_Operators.py
+# Modulus: 2
+
+
+
+
+# 7. Exponentiation (**)
+# Definition: It raises the first number to the power of the second.
+# Example:
+a = 2
+b = 3
+result = a ** b
+print("Exponentiation:", result)  # Output: 8
+
+#explantion is -  2*2*2 = (2*2=4 and 4*2=8)
+# PS C:\Users\GaneshMogal\Python-Learnings\Python Pratices\Python_operators> python Python_Arithmetic_Operators.py
+# Exponentiation: 8
+
+
+
